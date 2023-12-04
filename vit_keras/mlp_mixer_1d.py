1808c49a85a1299ab1e43c50aa85a27eab173f02
Muhammad Anas Raza <memanasraza@gmail.com>
2023-12-04 18:26:51 -0500

    added mixer 1d

---
+++ vit_keras/mlp_mixer_1d.py
@@ -0,0 +1,91 @@
+import keras_core as keras
+from keras_core import layers
+from keras_core import ops
+
+
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
+
+
+def posemb_sincos_1d(patches, temperature=10000, dtype="float32"):
+    n, dim = ops.shape(patches)[1], ops.shape(patches)[2]
+    n = ops.arange(n)
+    assert (dim % 2) == 0, "feature dimension must be multiple of 2 for sincos emb"
+    omega = ops.arange(dim // 2) / (dim // 2 - 1)
+    omega = ops.cast(omega, patches.dtype)
+    omega = 1.0 / (temperature**omega)
+    n = ops.expand_dims(ops.reshape(n, [-1]), 1)
+    n = ops.cast(n, patches.dtype)
+    n = n * ops.expand_dims(omega, 0)
+    pe = ops.concatenate((ops.sin(n), ops.cos(n)), 1)
+    return ops.cast(pe, dtype)
+
+
+class MLPMixerLayer(layers.Layer):
+    def __init__(self, num_patches, hidden_units, dropout_rate, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.mlp1 = keras.Sequential(
+            [
+                layers.Dense(units=num_patches, activation="gelu"),
+                layers.Dense(units=num_patches),
+                layers.Dropout(rate=dropout_rate),
+            ]
+        )
+        self.mlp2 = keras.Sequential(
+            [
+                layers.Dense(units=num_patches, activation="gelu"),
+                layers.Dense(units=hidden_units),
+                layers.Dropout(rate=dropout_rate),
+            ]
+        )
+        self.normalize = layers.LayerNormalization(epsilon=1e-6)
+
+    def build(self, input_shape):
+        return super().build(input_shape)
+
+    def call(self, inputs):
+        # Apply layer normalization.
+        x = self.normalize(inputs)
+        # Transpose inputs from [num_batches, num_patches, hidden_units] to [num_batches, hidden_units, num_patches].
+        x_channels = keras.ops.transpose(x, axes=(0, 2, 1))
+        # Apply mlp1 on each channel independently.
+        mlp1_outputs = self.mlp1(x_channels)
+        # Transpose mlp1_outputs from [num_batches, hidden_dim, num_patches] to [num_batches, num_patches, hidden_units].
+        mlp1_outputs = keras.ops.transpose(mlp1_outputs, axes=(0, 2, 1))
+        # Add skip connection.
+        x = mlp1_outputs + inputs
+        # Apply layer normalization.
+        x_patches = self.normalize(x)
+        # Apply mlp2 on each patch independtenly.
+        mlp2_outputs = self.mlp2(x_patches)
+        # Add skip connection.
+        x = x + mlp2_outputs
+        return x
+
+
+def Mixer1D(
+    seq_len,
+    patch_size,
+    num_classes,
+    dim,
+    depth,
+    channels=3,
+    hidden_units=64,
+    dropout_rate=0.
+):
+    assert seq_len % patch_size == 0
+    num_patches = seq_len // patch_size
+    patch_dim = channels * patch_size
+    i_p = layers.Input((seq_len, channels))
+    patches = layers.Reshape((-1, patch_dim))(i_p)
+    patches = layers.LayerNormalization()(patches)
+    patches = layers.Dense(dim)(patches)
+    patches = layers.LayerNormalization()(patches)
+    pos_embedding = posemb_sincos_1d(patches)
+    patches += pos_embedding
+    for _ in range(depth):
+        patches = MLPMixerLayer(num_patches, hidden_units, dropout_rate)(patches)
+    patches = layers.GlobalAveragePooling1D(name="avg_pool")(patches)
+    o_p = layers.Dense(num_classes)(patches)
+    return keras.Model(inputs=i_p, outputs=o_p)
