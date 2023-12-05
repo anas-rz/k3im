207d38af8475e5f5ba55e1fbb92dcf4f218741c2
Muhammad Anas Raza <memanasraza@gmail.com>
2023-12-04 21:44:13 -0500

    add gmlp for images

---
+++ vit_keras/gmlp.py
@@ -0,0 +1,146 @@
+import keras_core as keras
+from keras_core import layers
+
+
+
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
+
+
+class Patches(layers.Layer):
+    def __init__(self, patch_size, **kwargs):
+        super().__init__(**kwargs)
+        self.patch_size = patch_size
+
+    def call(self, x):
+        patches = keras.ops.image.extract_patches(x, self.patch_size)
+        batch_size = keras.ops.shape(patches)[0]
+        num_patches = keras.ops.shape(patches)[1] * keras.ops.shape(patches)[2]
+        patch_dim = keras.ops.shape(patches)[3]
+        out = keras.ops.reshape(patches, (batch_size, num_patches, patch_dim))
+        return out
+    
+class PositionEmbedding(keras.layers.Layer):
+    def __init__(
+        self,
+        sequence_length,
+        initializer="glorot_uniform",
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        if sequence_length is None:
+            raise ValueError("`sequence_length` must be an Integer, received `None`.")
+        self.sequence_length = int(sequence_length)
+        self.initializer = keras.initializers.get(initializer)
+
+    def get_config(self):
+        config = super().get_config()
+        config.update(
+            {
+                "sequence_length": self.sequence_length,
+                "initializer": keras.initializers.serialize(self.initializer),
+            }
+        )
+        return config
+
+    def build(self, input_shape):
+        feature_size = input_shape[-1]
+        self.position_embeddings = self.add_weight(
+            name="embeddings",
+            shape=[self.sequence_length, feature_size],
+            initializer=self.initializer,
+            trainable=True,
+        )
+
+        super().build(input_shape)
+
+    def call(self, inputs, start_index=0):
+        shape = keras.ops.shape(inputs)
+        feature_length = shape[-1]
+        sequence_length = shape[-2]
+        # trim to match the length of the input sequence, which might be less
+        # than the sequence_length of the layer.
+        position_embeddings = keras.ops.convert_to_tensor(self.position_embeddings)
+        position_embeddings = keras.ops.slice(
+            position_embeddings,
+            (start_index, 0),
+            (sequence_length, feature_length),
+        )
+        return keras.ops.broadcast_to(position_embeddings, shape)
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+    
+
+class gMLPLayer(layers.Layer):
+    def __init__(self, num_patches, embedding_dim, dropout_rate, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.channel_projection1 = keras.Sequential(
+            [
+                layers.Dense(units=embedding_dim * 2, activation="gelu"),
+                layers.Dropout(rate=dropout_rate),
+            ]
+        )
+
+        self.channel_projection2 = layers.Dense(units=embedding_dim)
+
+        self.spatial_projection = layers.Dense(
+            units=num_patches, bias_initializer="Ones"
+        )
+
+        self.normalize1 = layers.LayerNormalization(epsilon=1e-6)
+        self.normalize2 = layers.LayerNormalization(epsilon=1e-6)
+
+    def spatial_gating_unit(self, x):
+        # Split x along the channel dimensions.
+        # Tensors u and v will in the shape of [batch_size, num_patchs, embedding_dim].
+        u, v = keras.ops.split(x, indices_or_sections=2, axis=2)
+        # Apply layer normalization.
+        v = self.normalize2(v)
+        # Apply spatial projection.
+        v_channels = keras.ops.transpose(v, axes=(0, 2, 1))
+        v_projected = self.spatial_projection(v_channels)
+        v_projected = keras.ops.transpose(v_projected, axes=(0, 2, 1))
+        # Apply element-wise multiplication.
+        return u * v_projected
+
+    def call(self, inputs):
+        # Apply layer normalization.
+        x = self.normalize1(inputs)
+        # Apply the first channel projection. x_projected shape: [batch_size, num_patches, embedding_dim * 2].
+        x_projected = self.channel_projection1(x)
+        # Apply the spatial gating unit. x_spatial shape: [batch_size, num_patches, embedding_dim].
+        x_spatial = self.spatial_gating_unit(x_projected)
+        # Apply the second channel projection. x_projected shape: [batch_size, num_patches, embedding_dim].
+        x_projected = self.channel_projection2(x_spatial)
+        # Add skip connection.
+        return x + x_projected
+
+
+def gMLPModel(image_size, patch_size, embedding_dim, num_blocks, dropout_rate, num_classes, 
+                     positional_encoding=False, num_channels=3):
+    image_size = pair(image_size)
+    patch_size = pair(patch_size)
+    input_shape = (image_size[0], image_size[1], num_channels)
+    inputs = layers.Input(shape=input_shape)
+    num_patches = (image_size[0] // patch_size[0]) * (image_size[1] // patch_size[1])  # Size of the data array.
+
+    # Augment data.
+    # Create patches.
+    patches = Patches(patch_size)(inputs)
+    # Encode patches to generate a [batch_size, num_patches, embedding_dim] tensor.
+    x = layers.Dense(units=embedding_dim)(patches)
+    if positional_encoding:
+        x = x + PositionEmbedding(sequence_length=num_patches)(x)
+    # Process x using the module blocks.
+    for _ in range(num_blocks):
+        x = gMLPLayer(num_patches, embedding_dim, dropout_rate)(x)
+    # Apply global average pooling to generate a [batch_size, embedding_dim] representation tensor.
+    representation = layers.GlobalAveragePooling1D()(x)
+    # Apply dropout.
+    representation = layers.Dropout(rate=dropout_rate)(representation)
+    # Compute logits outputs.
+    logits = layers.Dense(num_classes)(representation)
+    # Create the Keras model.
+    return keras.Model(inputs=inputs, outputs=logits)
