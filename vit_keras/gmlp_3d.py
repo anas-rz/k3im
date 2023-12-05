86ef4c5777c4a6e3f19fd5db0d57148d86f77050
Muhammad Anas Raza <memanasraza@gmail.com>
2023-12-04 22:04:43 -0500

    gmlp for 3D data

---
+++ vit_keras/gmlp_3d.py
@@ -0,0 +1,101 @@
+import keras_core as keras
+from keras_core import layers
+from keras_core import ops
+
+
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
+
+
+class gMLPLayer(layers.Layer):
+    def __init__(self, num_patches, embedding_dim, dropout_rate, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.channel_projection1 = keras.Sequential(
+            [
+                layers.Dense(units=embedding_dim * 2, activation="gelu"),
+                layers.Dropout(rate=dropout_rate),
+            ]
+        )
+
+        self.channel_projection2 = layers.Dense(units=embedding_dim)
+
+        self.spatial_projection = layers.Dense(
+            units=num_patches, bias_initializer="Ones"
+        )
+
+        self.normalize1 = layers.LayerNormalization(epsilon=1e-6)
+        self.normalize2 = layers.LayerNormalization(epsilon=1e-6)
+
+    def spatial_gating_unit(self, x):
+        # Split x along the channel dimensions.
+        # Tensors u and v will in the shape of [batch_size, num_patchs, embedding_dim].
+        u, v = keras.ops.split(x, indices_or_sections=2, axis=2)
+        # Apply layer normalization.
+        v = self.normalize2(v)
+        # Apply spatial projection.
+        v_channels = keras.ops.transpose(v, axes=(0, 2, 1))
+        v_projected = self.spatial_projection(v_channels)
+        v_projected = keras.ops.transpose(v_projected, axes=(0, 2, 1))
+        # Apply element-wise multiplication.
+        return u * v_projected
+
+    def call(self, inputs):
+        # Apply layer normalization.
+        x = self.normalize1(inputs)
+        # Apply the first channel projection. x_projected shape: [batch_size, num_patches, embedding_dim * 2].
+        x_projected = self.channel_projection1(x)
+        # Apply the spatial gating unit. x_spatial shape: [batch_size, num_patches, embedding_dim].
+        x_spatial = self.spatial_gating_unit(x_projected)
+        # Apply the second channel projection. x_projected shape: [batch_size, num_patches, embedding_dim].
+        x_projected = self.channel_projection2(x_spatial)
+        # Add skip connection.
+        return x + x_projected
+
+
+def gMLP3D(
+    image_size,
+    image_patch_size,
+    frames,
+    frame_patch_size,
+    num_classes,
+    dim,
+    depth,
+    hidden_units,
+    dropout_rate,
+    channels=3,
+):
+    image_height, image_width = pair(image_size)
+    patch_height, patch_width = pair(image_patch_size)
+
+    assert (
+        image_height % patch_height == 0 and image_width % patch_width == 0
+    ), "Image dimensions must be divisible by the patch size."
+    assert (
+        frames % frame_patch_size == 0
+    ), "Frames must be divisible by the frame patch size"
+
+    nf, nh, nw = (
+        frames // frame_patch_size,
+        image_height // patch_height,
+        image_width // patch_width,
+    )
+    patch_dim = channels * patch_height * patch_width * frame_patch_size
+
+    i_p = layers.Input((frames, image_height, image_width, channels))
+    tubelets = layers.Reshape(
+        (frame_patch_size, nf, patch_height, nh, patch_width, nw, channels)
+    )(i_p)
+    tubelets = ops.transpose(tubelets, (0, 2, 4, 6, 1, 3, 5, 7))
+    tubelets = layers.Reshape((nf, nh, nw, -1))(tubelets)
+    tubelets = layers.LayerNormalization()(tubelets)
+    tubelets = layers.Dense(dim)(tubelets)
+    tubelets = layers.LayerNormalization()(tubelets)
+    tubelets = layers.Reshape((-1, dim))(tubelets)
+    num_patches = ops.shape(tubelets)[1]
+    for _ in range(depth):
+        tubelets = gMLPLayer(num_patches, hidden_units, dropout_rate)(tubelets)
+    tubelets = layers.GlobalAveragePooling1D(name="avg_pool")(tubelets)
+    o_p = layers.Dense(num_classes)(tubelets)
+
+    return keras.Model(inputs=i_p, outputs=o_p)
