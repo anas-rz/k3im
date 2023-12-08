72ddac986ad465f912e22e21dc556150c5674ee3
Muhammad Anas Raza <memanasraza@gmail.com>
2023-12-08 15:31:19 -0500

    added smoe

--- k3im/smoe_3d.py
+++ k3im/smoe_3d.py
@@ -3,10 +3,6 @@ from keras import layers
 from keras import ops
 
 
-def pair(t):
-    return t if isinstance(t, tuple) else (t, t)
-
-
 class ClassTokenPositionEmb(layers.Layer):
     def __init__(self, sequence_length, output_dim, **kwargs):
         super().__init__(**kwargs)
@@ -27,6 +23,10 @@ class ClassTokenPositionEmb(layers.Layer):
         positions = ops.arange(start=0, stop=(length + 1), step=1)
         embedded_positions = self.position_embeddings(positions)
         return patches + embedded_positions
+
+
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
 
 
 class MultiExpertLayer(layers.Layer):
@@ -214,46 +214,87 @@ def SoftMoEEncoder(num_layers, d_model, nhead, **kwargs):
     return _apply
 
 
-def SMOE_Model(
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
+
+
+class ClassTokenPositionEmb(layers.Layer):
+    def __init__(self, sequence_length, output_dim, **kwargs):
+        super().__init__(**kwargs)
+        self.position_embeddings = layers.Embedding(
+            input_dim=(sequence_length + 1), output_dim=output_dim
+        )
+        self.class_token = self.add_weight(
+            shape=[1, 1, output_dim], initializer="random_normal"
+        )
+        self.sequence_length = sequence_length
+        self.output_dim = output_dim
+
+    def call(self, inputs):
+        batch, length = ops.shape(inputs)[0], ops.shape(inputs)[1]
+
+        cls_token = ops.repeat(self.class_token, batch, axis=0)
+        patches = ops.concatenate([inputs, cls_token], axis=1)
+        positions = ops.arange(start=0, stop=(length + 1), step=1)
+        embedded_positions = self.position_embeddings(positions)
+        return patches + embedded_positions
+
+
+def SMOE3DModel(
     image_size,
-    patch_size,
+    image_patch_size,
+    frames,
+    frame_patch_size,
     num_classes,
     dim,
     depth,
     heads,
     mlp_dim,
+    pool,
     channels=3,
-    pool="mean",
+    dim_head=64,
 ):
     image_height, image_width = pair(image_size)
-    patch_height, patch_width = pair(patch_size)
+    patch_height, patch_width = pair(image_patch_size)
 
     assert (
         image_height % patch_height == 0 and image_width % patch_width == 0
     ), "Image dimensions must be divisible by the patch size."
+    assert (
+        frames % frame_patch_size == 0
+    ), "Frames must be divisible by the frame patch size"
+
     assert pool in {
         "cls",
         "mean",
     }, "pool type must be either cls (cls token) or mean (mean pooling)"
-    patch_dim = channels * patch_height * patch_width
 
-    i_p = layers.Input((image_height, image_width, channels))
-    patches = ops.image.extract_patches(i_p, (patch_height, patch_width))
-    patches = layers.Reshape((-1, patch_dim))(patches)
-    patches = layers.LayerNormalization()(patches)
-    patches = layers.Dense(dim)(patches)
-    patches = layers.LayerNormalization()(patches)
-    num_patches = ops.shape(patches)[1]
-    patches = ClassTokenPositionEmb(num_patches, dim)(patches)
-    patches = SoftMoEEncoder(
+    nf, nh, nw = (
+        frames // frame_patch_size,
+        image_height // patch_height,
+        image_width // patch_width,
+    )
+    patch_dim = channels * patch_height * patch_width * frame_patch_size
+
+    i_p = layers.Input((frames, image_height, image_width, channels))
+    tubelets = layers.Reshape(
+        (frame_patch_size, nf, patch_height, nh, patch_width, nw, channels)
+    )(i_p)
+    tubelets = ops.transpose(tubelets, (0, 2, 4, 6, 1, 3, 5, 7))
+    tubelets = layers.Reshape((nf, nh, nw, -1))(tubelets)
+    tubelets = layers.LayerNormalization()(tubelets)
+    tubelets = layers.Dense(dim)(tubelets)
+    tubelets = layers.LayerNormalization()(tubelets)
+    tubelets = layers.Reshape((-1, dim))(tubelets)
+    num_patches = ops.shape(tubelets)[1]
+    tubelets = ClassTokenPositionEmb(num_patches, dim)(tubelets)
+    tubelets = patches = SoftMoEEncoder(
         num_layers=depth, d_model=dim, nhead=heads, dim_feedforward=mlp_dim
-    )(patches)
-
-    if pool == "cls":
-        patches = patches[:, -1]
-    elif pool == "mean":
-        patches = layers.GlobalAveragePooling1D(name="max_pool")(patches)
-
-    o_p = layers.Dense(num_classes)(patches)
+    )(tubelets)
+    if pool == "mean":
+        tubelets = layers.GlobalAveragePooling1D(name="avg_pool")(tubelets)
+    else:
+        tubelets = tubelets[:, -1]
+    o_p = layers.Dense(num_classes)(tubelets)
 
     return keras.Model(inputs=i_p, outputs=o_p)
